283878e1c5ef0e08a78883146fc5a5ade031df83
Ayush <aayushtiwari071@gmail.com>
2023-09-05 06:58:30 +0530

    payment gateway

---
+++ scrip.js
@@ -0,0 +1,44 @@
+const images = [...document.querySelectorAll('.image')];
+
+// popup
+
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close-btn');
+const imageName = document.querySelector('.image-name');
+const largeImage = document.querySelector('.large-image');
+const imageIndex = document.querySelector('.index');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+
+let index = 0; // will track our current image;
+
+images.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        updateImage(i);
+        popup.classList.toggle('active');
+    })
+})
+
+const updateImage = (i) => {
+    let path = `qr.jpeg`;
+    largeImage.src = path;
+    // imageName.innerHTML = path;
+    // imageIndex.innerHTML = `0${i+1}`;
+    // index = i;
+}
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.toggle('active');
+})
+
+// leftArrow.addEventListener('click', () => {
+//     if(index > 0){
+//         updateImage(index - 1);
+//     }
+// })
+
+// rightArrow.addEventListener('click', () => {
+//     if(index < images.length - 1){
+//         updateImage(index + 1);
+//     }
+// })
